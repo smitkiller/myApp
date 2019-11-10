90b0ac1897f9f2c894fd769b3fbeef1b3cb0ff75
smitkiller <30369377+smitkiller@users.noreply.github.com>
2019-11-10 20:15:46 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 
 let config = {
-    apiKey: "AIzaSyChvAf--oVxeQ_l2guJC5WVlRqdbQACesQ",
-    authDomain: "cre-redux.firebaseapp.com",
-    databaseURL: "https://cre-redux.firebaseio.com",
-    projectId: "cre-redux",
-    storageBucket: "cre-redux.appspot.com",
-    messagingSenderId: "585333449053"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 
 let app=firebase.initializeApp(config);
